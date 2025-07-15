33cee20476cc3e4e23a9dccbd44d00c4d5b7241c
Rashibangar <bangarrashi@gmail.com>
2025-07-15 10:42:24 +0530

    Lecture-6

---
+++ Lec-6/write/index.js
@@ -0,0 +1,14 @@
+// const fs = require("fs");
+// fs.writeFile("../demo.txt","hello g27",function(err){
+//     if(err) return console.log(err);
+//     console.log("success!!")
+// })
+
+const fs = require("fs");
+fs.writeFile("../demo2.txt","hello Rashi",function(err){
+    if(err) return console.log(err);
+    console.log("success!!")
+})
+
+
+
